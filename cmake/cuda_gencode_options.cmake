0c6c18caee88e945d71eca8cbadaefa241fa36b9
Benjamin Ummenhofer <ummenhof@cs.uni-freiburg.de>
2017-12-03 16:17:37 +0100

    updated cuda gencode options

--- cmake/cuda_gencode_options.cmake
+++ cmake/cuda_gencode_options.cmake
@@ -34,6 +34,16 @@ if( GENERATE_MAXWELL_SM52_CODE )
         list( APPEND CUDA_GENCODE_STRING "-gencode=arch=compute_52,code=sm_52" )
 endif()
 
+option( GENERATE_PASCAL_SM60_CODE "This will generate code for the Pascal GPU architecture (sm_60)" ON )
+if( GENERATE_PASCAL_SM60_CODE )
+        list( APPEND CUDA_GENCODE_STRING "-gencode=arch=compute_60,code=sm_60" )
+endif()
+
+option( GENERATE_PTX60_CODE "This will generate PTX 6.0 code" OFF )
+if( GENERATE_PTX60_CODE )
+        list( APPEND CUDA_GENCODE_STRING "-gencode=arch=compute_60,code=compute_60" )
+endif()
+
 option( GENERATE_PASCAL_SM61_CODE "This will generate code for the Pascal GPU architecture (sm_61)" ON )
 if( GENERATE_PASCAL_SM61_CODE )
         list( APPEND CUDA_GENCODE_STRING "-gencode=arch=compute_61,code=sm_61" )
@@ -42,5 +52,15 @@ endif()
 option( GENERATE_PTX61_CODE "This will generate PTX 6.1 code" OFF )
 if( GENERATE_PTX61_CODE )
         list( APPEND CUDA_GENCODE_STRING "-gencode=arch=compute_61,code=compute_61" )
+endif()
+
+option( GENERATE_VOLTA_SM70_CODE "This will generate code for the Pascal GPU architecture (sm_70)" OFF )
+if( GENERATE_VOLTA_SM70_CODE )
+        list( APPEND CUDA_GENCODE_STRING "-gencode=arch=compute_70,code=sm_70" )
+endif()
+
+option( GENERATE_PTX70_CODE "This will generate PTX 7.0 code" OFF )
+if( GENERATE_PTX70_CODE )
+        list( APPEND CUDA_GENCODE_STRING "-gencode=arch=compute_70,code=compute_70" )
 endif()
 
