20d5aac90d9b4acf396d4d4d2d69c7e186686b33
Eli Chan <dr.elichan@yandex.com>
2018-10-02 10:56:20 +0800

    xxx

--- test/runme.sh
+++ test/runme.sh
@@ -1,6 +1,10 @@
+export TF_CPP_MIN_LOG_LEVEL=2
+log_file="results.log"
+> $log_file
+tail -f results.log & 
 for f in *.py; do
 	echo "$f is starting"
-	python3 $f
+	python3 $f >> $log_file 2>&1
 	echo "$f is done"
 	sleep 1
 done 
